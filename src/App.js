41120fbb1b37937d78619231c0b08b7ea0226010
evolvewd <vincenzo.bevivino@gmail.com>
2022-01-18 21:38:55 +0100

    structuring folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Article, Brand, Cta } from "./components";
+import { Blog, Evolvewd, Features, Footer, Header, Possibility } from "./containers";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Article />
+      <Brand />
+      <Cta />
+      <Blog />
+      <Cta />
+      <Evolvewd />
+      <Features />
+      <Footer />
+      <Header />
+      <Possibility />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/containers/evolvewd/Evolvewd.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./evolvewd.css";
+
+const Evolvewd = () => {
+  return <div>Evolvewd</div>;
+};
+
+export default Evolvewd;
